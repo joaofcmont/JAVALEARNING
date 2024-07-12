cea8e00ca7ff702755ce7331fa6b4393ec02ddfd
joaomonteiro <jfcmo98647@gmail.com>
2024-07-12 14:31:18 +0100

    Matrizes

---
+++ VECTORS/Matrizes/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [][] mat = new int[n][n];
+
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[i].length; j++){
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Main Diagonal");
+        for(int i=0; i<mat.length; i++){
+            System.out.println(mat[i][i] + " ");
+            }
+        System.out.println();
+
+        int negativeNumbers =0;
+
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[i].length; j++) {
+                if(mat[i][j] < 0){
+                    negativeNumbers++;
+                }
+            }
+        }
+
+        System.out.println("Negative Numbers: " + negativeNumbers);
+
+        sc.close();
+    }
+}
