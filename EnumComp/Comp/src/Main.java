00fc96620cd623f8adf6003350b1ec26459237c0
joaomonteiro <jfcmo98647@gmail.com>
2024-07-16 17:54:51 +0100

    Worker class and compositions

---
+++ EnumComp/Comp/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //composição -> tem um ou tem varios
+        //organizacao; coesão; flexibilidade; reuso
+        // faz parte de algo, algo dentro de outra coisa
+        // composição pode ocorrer com entidades ou serviços
+
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+}
