ca4fe488ee8c30e34a713c3886c6615aa492dc7b
joaomonteiro <jfcmo98647@gmail.com>
2024-07-16 09:23:45 +0100

    Enum

---
+++ EnumComp/Enum - Composition/src/Order.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+
+public class Order {
+
+    private Integer id;
+    private Date moment;
+    private OrderStatus status;
+
+    public Order(Integer id, OrderStatus status, Date moment) {
+        this.id = id;
+        this.status = status;
+        this.moment = moment;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", moment=" + moment +
+                ", status=" + status +
+                '}';
+    }
+}
