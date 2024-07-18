1f8f846bac517cf39722ef876408b9385a946d23
joaomonteiro <jfcmo98647@gmail.com>
2024-07-18 17:15:52 +0100

    final method and classes

---
+++ InheritancePolymorphism/ClassesMethosFinal/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //final
+        // aplicada a classe evita que a classe seja herdada
+        // "public final class SavingsAccount{"
+
+        //final aplicada ao método evita que seja sobreposto
+        // public final void withdraw(double amout){}
+
+        // fianl server para SEGURANÇA, PERFORMANCE,
+        // com final a classe ou método
+        // é mais rápida a ser analizada em tempo
+        // de execução
+    }
+}
