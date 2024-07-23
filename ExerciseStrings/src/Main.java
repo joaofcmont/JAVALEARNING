2e1fc7c2cdd5e179ed80ec31c2a289a473c01d5d
joaomonteiro <jfcmo98647@gmail.com>
2024-07-23 17:24:41 +0100

    Exercise String manipulating

--- ExerciseStrings/src/Main.java
+++ ExerciseStrings/src/Main.java
@@ -13,8 +13,7 @@ public class Main {
     }
 
 
-    public static String upndown(String s) {
-
+    public static String upndown(String s) throws IllegalArgumentException{
         StringBuilder result = new StringBuilder();
         boolean upper = true;
 
