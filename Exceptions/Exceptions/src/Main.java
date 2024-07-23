ca94005f40a12b07fdb354b8a689eee46c9a730d
joaomonteiro <jfcmo98647@gmail.com>
2024-07-23 16:42:28 +0100

    Adding finaly block

--- Exceptions/Exceptions/src/Main.java
+++ Exceptions/Exceptions/src/Main.java
@@ -5,7 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        method1();
+        System.out.println("FIM DO PROGRAMA");
 
+    }
+
+    public static void method1(){
+        System.out.println("*****Método 1 STARTED****");
+        method2();
+        System.out.println("*****Método 1 END****");
+
+    }
+
+    public static void method2(){
+        System.out.println("Método 2 STARTED ");
         Scanner sc = new Scanner(System.in);
 
         try{
@@ -14,12 +27,16 @@ public class Main {
             System.out.println(vect[position]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid Position!");
+            //imprimir o rastreamento do stack
+            e.printStackTrace();
+            sc.next();
         } catch (InputMismatchException e){
             System.out.println("Input error");
+        }finally{
+
         }
 
-
-        System.out.println("FIM DO PROGRAMA");
+        System.out.println("*****Método 2 END****");
 
         sc.close();
     }
