746b284f8336432d0797f9d8b2797d5999f1a4c3
joaomonteiro <jfcmo98647@gmail.com>
2024-07-23 16:53:15 +0100

    Personalized exception

--- Exceptions/Exceptions/src/expections/DomainException.java
+++ Exceptions/Exceptions/src/expections/DomainException.java
@@ -2,6 +2,10 @@ package expections;
 
 public class DomainException extends Exception{
 
+    private static final long serialVersionUID = 1L;
 
+    public DomainException(String msg){
+        super(msg);
+    }
 
 }
