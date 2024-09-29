c424c6ca287d99fe543c6e08b1f00c0c026c24ef
joaomonteiro <jfcmo98647@gmail.com>
2024-09-29 15:58:38 +0100

    Buffer Reader Learning

--- Files/Files.java
+++ Files/Files.java
@@ -1,25 +1,34 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Files {
 
     public static void main(String[] args) {
-        File file = new File("/Users/joaomonteiro/test.java");
-        Scanner sc = null;
+        String path= "/Users/joaomonteiro/test.java";
+        BufferedReader bufferedReader = null;
+        FileReader fileReader = null;
 
         try{
-            sc = new Scanner(file);
-            while(sc.hasNextLine()){
-                System.out.println(sc.nextLine());
+            fileReader = new FileReader(path);
+            bufferedReader = new BufferedReader(fileReader);
+
+            String line = bufferedReader.readLine();
+
+            while(line != null){
+                System.out.println(line);
+                line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
         finally {
-            if(sc != null){
-                sc.close();
+            try {
+                if(bufferedReader != null)
+                    bufferedReader.close();
+                if(fileReader != null)
+                    fileReader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
 
